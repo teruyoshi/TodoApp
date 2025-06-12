1823612a56d2d8ba7598c01b4c51765882c03eb1
teruyoshi <teruyoshi.sakaya@gmail.com>
2025-06-13 07:41:13 +0900

    Use repository in todo create usecase

--- go/internal/router/todo.go
+++ go/internal/router/todo.go
@@ -1,14 +1,27 @@
 package router
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/go-chi/chi/v5"
 
 	todoCreateHandler "github.com/teruyoshi/todoApp/internal/features/todoCreate/handler"
+	todoCreateRepoMysql "github.com/teruyoshi/todoApp/internal/features/todoCreate/repository/mysql"
 	todoCreateUseCase "github.com/teruyoshi/todoApp/internal/features/todoCreate/usecase"
 )
 
 func RegisterTodoRoutes(r chi.Router) {
-	uc := todoCreateUseCase.NewTodoCreateUseCase()
+	dsn := fmt.Sprintf("%s:%s@tcp(db:3306)/%s?parseTime=true",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	repo, err := todoCreateRepoMysql.NewTodoRepository(dsn)
+	if err != nil {
+		panic(err)
+	}
+	uc := todoCreateUseCase.NewTodoCreateUseCase(repo)
 	handler := todoCreateHandler.NewTodoCreateHandler(uc)
 
 	r.Post("/todos/", handler.Create)
