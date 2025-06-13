ec3c6509b9afced60866531b4063dbbe406709e5
teruyoshi <teruyoshi.sakaya@gmail.com>
2025-06-13 17:36:25 +0900

    動くようにするために日付のDB構造を一旦文字列で表した

--- go/internal/features/todoCreate/repository/mysql/t_todo.go
+++ go/internal/features/todoCreate/repository/mysql/t_todo.go
@@ -4,8 +4,8 @@ type TTodo struct {
 	ID              uint   `gorm:"column:id;primaryKey;autoIncrement"`
 	TodoTitle       string `gorm:"column:title;type:varchar(20)"`
 	TodoDescription string `gorm:"column:description;type:varchar(200)"`
-	TodoDateFrom    string `gorm:"column:date_from;type:date"`
-	TodoDateTo      string `gorm:"column:date_to;type:date"`
+	TodoDateFrom    string `gorm:"column:date_from;type:varchar(200)"`
+	TodoDateTo      string `gorm:"column:date_to;type:varchar(200)"`
 }
 
 func (TTodo) TableName() string {
