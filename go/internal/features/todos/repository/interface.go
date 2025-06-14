eae5a5c6b93a11ea5b13a5957b6d28c07be4a517
teruyoshi <teruyoshi.sakaya@gmail.com>
2025-06-14 17:58:06 +0900

    Move fetch method to todos repository

--- go/internal/features/todos/repository/interface.go
+++ go/internal/features/todos/repository/interface.go
@@ -4,4 +4,5 @@ import "github.com/teruyoshi/todoApp/internal/features/todos/entity"
 
 type TodoRepository interface {
 	Create(t entity.Todo) (entity.Todo, error)
+	Fetch() ([]entity.Todo, error)
 }
