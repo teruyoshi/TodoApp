a18be69f8872d6abcc8781c3047bd20823eafc92
teruyoshi <teruyoshi.sakaya@gmail.com>
2025-07-24 06:57:05 +0900

    ほとんど進んでいなかった過去の features/todoFetch を削除した  #37

--- go/internal/features/todoFetch/repository/interface.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "github.com/teruyoshi/todoApp/internal/features/todos/entity"
-
-// TodoRepository provides methods to fetch todos.
-type TodoRepository interface {
-	Fetch() ([]entity.Todo, error)
-}
