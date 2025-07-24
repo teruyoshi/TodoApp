65521d49c8a805365b7f9774626a471a086afaf1
teruyoshi <teruyoshi.sakaya@gmail.com>
2025-07-25 05:49:28 +0900

    playwright のコンテナ化を模索中 #37

---
+++ e2e/tests/todo.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test'
+
+test.describe.configure({ mode: 'parallel' })
+
+test('/todo ページ TODO を追加して表示される', async ({ page }) => {
+  await page.goto('http://hostmachine:5173/todo')
+
+  page.on('request', (request) => {
+    console.log('>>', request.method(), request.url())
+  })
+  page.on('response', (response) => {
+    console.log('<<', response.status(), response.url())
+  })
+
+  await page.getByLabel('タイトル').fill('TODO のタイトル')
+  await page.getByLabel('説明').fill('TODO の説明')
+  await page.getByRole('button', { name: '追加' }).click()
+
+  await expect(page.getByRole('listitem')).toHaveText('TODO のタイトル')
+  await expect(page.getByRole('listitem')).toHaveText('TODO の説明')
+})
+
+// test('/todo ページでタイトルが表示されている', async ({ page }) => {
+//   await page.goto('http://localhost:5173/todo')
+
+//   await expect(page).toHaveTitle('TODO管理アプリ')
+// })
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/')
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click()
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(
+//     page.getByRole('heading', { name: 'Installation' })
+//   ).toBeVisible()
+// })
--- front/src/features/todoView/components/TodoList.tsx
+++ front/src/features/todoView/components/TodoList.tsx
@@ -10,11 +10,6 @@ interface Todo {
 type Todos = Todo[]
 
 function TodoList() {
-  // const todos: Todos = [
-  //   { id: 1, title: '国語の勉強', description: '音読と漢字の宿題' },
-  //   { id: 2, title: '数学の勉強', description: '計算ドリル' },
-  //   { id: 3, title: '英語の勉強', description: '単語帳の暗記' },
-  // ]
   const { data } = useFetchTodosQuery()
 
   return (
